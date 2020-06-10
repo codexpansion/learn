584a3e25ee71518396c8ef5e7d945ad0222a7568
codexpansion <32369113+codexpansion@users.noreply.github.com>
2020-06-10 20:25:51 +0300

    Update p2_2.cpp
    #include <iostream>
    #include <string>
    #include <vector>
    
    int main()
    {
      setbuf(stdout, NULL);    
      
      float sum = 0;
      int i = 0;
      int num;
      
      
      std::string buffer;
      
      do {
            
            printf("numv[%d] = ", (int) i);       
            scanf("%d", &num);
            
            
            if (num != 10) {          
                if (num < 0) { sum +=num;}
                i++;
            };
           
        } while (num != 10);
      
      
       
        
        
       
        
      
        printf ("summ minus = %f", sum);
      
    }

--- p2_2.cpp
+++ p2_2.cpp
@@ -8,23 +8,23 @@ int main()
   
   float sum = 0;
   int i = 0;
-  float num;
-  float eps = 0.0001;
+  int num;
+  
   
   std::string buffer;
   
   do {
         
         printf("numv[%d] = ", (int) i);       
-        scanf("%f", &num);
+        scanf("%d", &num);
         
         
-        if (abs(num - 10.0) > eps) {          
+        if (num != 10) {          
             if (num < 0) { sum +=num;}
             i++;
         };
        
-    } while (abs(num - 10.0) > eps);
+    } while (num != 10);
   
   
    
